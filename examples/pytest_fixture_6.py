7be2e188fc29f588999d3a3882c96e2f48ebfa0e
MGalganek <galganek.mateusz@gmail.com>
2014-02-07 08:44:32 +0100

    py.testfixtures update

---
+++ examples/pytest_fixture_6.py
@@ -0,0 +1,21 @@
+
+import pytest
+import smtplib
+
+
+@pytest.fixture(scope="module")
+def smtp():
+    return smtplib.SMTP("merlinux.eu")
+
+
+def test_ehlo(smtp):
+    response = smtp.ehlo()
+    assert response[0] == 250
+    assert "merlinux" in response[1]
+    assert 0  # for demo purposes
+
+
+def test_noop(smtp):
+    response = smtp.noop()
+    assert response[0] == 250
+    assert 0  # for demo purposes
