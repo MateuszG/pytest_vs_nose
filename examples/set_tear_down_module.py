674c6ae3a0b91d2f3359595fc1d8c6d22c901593
MGalganek <galganek.mateusz@gmail.com>
2014-02-07 07:51:37 +0100

    Adeed few fixes

---
+++ examples/set_tear_down_module.py
@@ -0,0 +1,7 @@
+def setup_module(module):
+""" setup any state specific to the execution of the given module."""
+
+def teardown_module(module):
+""" teardown any state that was previously setup with a setup_module
+method.
+"""
